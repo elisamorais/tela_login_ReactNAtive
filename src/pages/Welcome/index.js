6eb1eca38e406f5611c667677a9d0a52574fdb12
Elisa Morais <elisa.morais74@gmail.com>
2022-10-03 01:45:58 -0300

    Configuração das rotas

---
+++ src/pages/Welcome/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Welcome() {
+    return ( 
+        <View>
+            <Text>Tela Bem-Vindo!</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    
+})
